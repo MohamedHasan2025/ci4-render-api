fe2085f200e92727c27449820b752bf9fb6d31fd
MohamedHasan2025 <Mohamed.ashraff@daw.ae>
2026-01-05 13:12:47 +0400

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,4 +9,3 @@ $routes->get('/', 'Home::index');
 
 
 $routes->post('get-availabilities', 'ApiController::sendAvailability');
-$routes->get('get-availabilities', 'ApiController::sendAvailability'); // optional for GET
