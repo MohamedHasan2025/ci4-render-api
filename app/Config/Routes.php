7398465d119a30e61d3ea2aa4bbb064db8a772a6
Mohamed Ashraff <mohamed.ashraff@daw.org.ae>
2026-01-05 13:03:48 +0400

    Add ApiController and update routes for get-availabilities endpoint

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+
+$routes->post('get-availabilities', 'ApiController::sendAvailability');
+$routes->get('get-availabilities', 'ApiController::sendAvailability'); // optional for GET
