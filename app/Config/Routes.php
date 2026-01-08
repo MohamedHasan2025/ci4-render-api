8fd1578f9b6b3677dc35e37c0b5c80ff12f1a8a6
MohamedHasan2025 <Mohamed.ashraff@daw.ae>
2026-01-08 12:52:38 +0400

    Update API routes to include versioning

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,10 +9,10 @@ $routes->get('/', 'Home::index');
 
 $routes->get('1/get-availabilities', 'ApiController::sendAvailability');
 
-$routes->post('/reserve', 'ApiController::reserveAvailability');
+$routes->post('1/reserve', 'ApiController::reserveAvailability');
 
-$routes->post('/cancel-reservation', 'ApiController::cancelReservation');
+$routes->post('1/cancel-reservation', 'ApiController::cancelReservation');
 
-$routes->post('/book', 'ApiController::bookReservation');
+$routes->post('1/book', 'ApiController::bookReservation');
     
-$routes->post('/cancel-booking', 'ApiController::cancelReservation');
+$routes->post('1/cancel-booking', 'ApiController::cancelReservation');
