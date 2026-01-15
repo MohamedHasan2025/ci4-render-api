844458ef87c699065533fb3bc7dac490ddbee939
MohamedHasan2025 <Mohamed.ashraff@daw.ae>
2026-01-15 11:24:35 +0400

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,12 +9,22 @@ $routes->get('/', 'Home::index');
 
 $routes->get('1/get-availabilities', 'ApiController::sendAvailability');
 
+$routes->get('1/get-availabilities/', 'ApiController::sendAvailability');
+
 $routes->post('1/reserve', 'ApiController::reserveAvailability');
+
+$routes->post('1/reserve/', 'ApiController::reserveAvailability');
 
 $routes->post('1/cancel-reservation', 'ApiController::cancelReservation');
 
+$routes->post('1/cancel-reservation/', 'ApiController::cancelReservation');
+
 $routes->post('1/book', 'ApiController::bookReservation');
+
+$routes->post('1/book/', 'ApiController::bookReservation');
     
 $routes->post('1/cancel-booking', 'ApiController::cancelReservation');
+    
+$routes->post('1/cancel-booking/', 'ApiController::cancelReservation');
 
 $routes->setAutoRoute(true);
