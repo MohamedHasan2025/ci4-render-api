211f14fa073497fbc43a6629dc2836f7a1294459
MohamedHasan2025 <Mohamed.ashraff@daw.ae>
2026-01-19 09:23:20 +0400

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,14 +13,14 @@ $routes->get('1/get-availabilities/', 'ApiController::sendAvailability');
 //$routes->get('1/get-availabilities(/)?', 'ApiController::sendAvailability');
 //$routes->get('1/get-availabilities/', 'ApiController::sendAvailability');
 
-$routes->post('1/reserve(/)?', 'ApiController::reserveAvailability');
+//$routes->post('1/reserve(/)?', 'ApiController::reserveAvailability');
 $routes->post('1/reserve/', 'ApiController::reserveAvailability');
 
-$routes->post('1/cancel-reservation(/)?', 'ApiController::cancelReservation');
+//$routes->post('1/cancel-reservation(/)?', 'ApiController::cancelReservation');
 $routes->post('1/cancel-reservation/', 'ApiController::cancelReservation');
 
-$routes->post('1/book(/)?', 'ApiController::bookReservation');
+//$routes->post('1/book(/)?', 'ApiController::bookReservation');
 $routes->post('1/book/', 'ApiController::bookReservation');
 
-$routes->post('1/cancel-booking(/)?', 'ApiController::cancelReservation');
+//$routes->post('1/cancel-booking(/)?', 'ApiController::cancelReservation');
 $routes->post('1/cancel-booking/', 'ApiController::cancelReservation');
