bd9035910ac6867dd922eac670b8f8c034f8857f
MohamedHasan2025 <Mohamed.ashraff@daw.ae>
2026-01-19 09:49:05 +0400

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,19 +8,19 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 //$routes->get('/1/get-availabilities(/)?', 'ApiController::sendAvailability');
-$routes->get('1/get-availabilities/', 'ApiController::sendAvailability');
+$routes->get('1/get-availabilities', 'ApiController::sendAvailability');
 
 //$routes->get('1/get-availabilities(/)?', 'ApiController::sendAvailability');
 //$routes->get('1/get-availabilities/', 'ApiController::sendAvailability');
 
 //$routes->post('1/reserve(/)?', 'ApiController::reserveAvailability');
-$routes->post('1/reserve/', 'ApiController::reserveAvailability');
+$routes->post('1/reserve', 'ApiController::reserveAvailability');
 
 //$routes->post('1/cancel-reservation(/)?', 'ApiController::cancelReservation');
-$routes->post('1/cancel-reservation/', 'ApiController::cancelReservation');
+$routes->post('1/cancel-reservation', 'ApiController::cancelReservation');
 
 //$routes->post('1/book(/)?', 'ApiController::bookReservation');
-$routes->post('1/book/', 'ApiController::bookReservation');
+$routes->post('1/book', 'ApiController::bookReservation');
 
 //$routes->post('1/cancel-booking(/)?', 'ApiController::cancelReservation');
-$routes->post('1/cancel-booking/', 'ApiController::cancelReservation');
+$routes->post('1/cancel-booking', 'ApiController::cancelReservation');
